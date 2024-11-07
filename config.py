cf1d1b6d89e691f18cce7db1cc8c4d8205aa6ea8
Elektroblock <52217834+Elektroblock@users.noreply.github.com>
2024-11-07 22:26:46 +0100

    Auto read files added

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
-api_key = '---UR_API_KEY---'
-server_url = "http://127.0.0.1"
+# General Configuration
 DEBUG = True
-use_webclient = True
+
+# Upload Configuration
+# More information on: https://github.com/Elektroblock/StratoWeb/settings
+use_webclient = True # Enables upload to chosen Server
+api_key = 'XUVCHYbp2y6NMjjELjDERUQtdaLRsTsFshaJgVk585pbcWVT9PuGT7yvm2xHHMXM' # Must be the same betwene Client and Server!
+server_url = "https://api.mlstrato.de"
+input_filepath = r'C:\Users\Julian\PycharmProjects\MLS-receive\out'
--- receiver.py
+++ receiver.py
@@ -6,6 +6,7 @@ import json
 from threading import Thread
 import os
 
+from config import input_filepath
 
 imageBytes = b''
 
@@ -13,52 +14,71 @@ from PIL import Image, ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True # Allow to safe bad images
 
 def wait_for_data(webserver_queue):
-
+    processed_files = set()
     #exit()
-    files = glob.glob('out/*')
+    while True:
+        # Liste aller Dateien im Ordner, die auf '.bin' enden
+        current_files = [f for f in os.listdir(input_filepath) if f.endswith('.bin')]
 
-    for i in range(0, len(files)):
-        filename = "out/" + "output" + str(i) + ".bin"
+        # Dateien nach Zahl im Namen sortieren
+        current_files.sort(key=lambda x: int(x.split('.')[0]))
+        # Neue Dateien in der richtigen Reihenfolge ausgeben
+        for file in current_files:
+            if file not in processed_files:
 
-        print(filename)
+                filename = input_filepath + "/" + file
 
-        f = open(filename, "rb")
+                print(filename)
 
-        input_raw_data = f.read()
+                f = open(filename, "rb")
 
-        f.close()
+                input_raw_data = f.read()
+
+                f.close()
+                print(input_raw_data)
+
+                if input_raw_data[0] == 118:  # test for v character at beginning (Image Data)
+                    print("Image Data")
+                    handle_image(input_raw_data[1:], False, webserver_queue)
+
+                elif input_raw_data[0] == 115:  # test for s character at begingin (Sensor Data)
+                    print("Sensor Data")
+                    handle_sensor(input_raw_data.decode("utf-8"), webserver_queue)
+
+                elif input_raw_data[0] == 120:  # test for x character at beginning (last Image Data)F
+                    print("last Image Data")
+                    handle_image(input_raw_data[1:], True, webserver_queue)
 
 
-        if input_raw_data[0] == 118:  # test for v character at beginning (Image Data)
-            print("Image Data")
-            handle_image(input_raw_data[1:], False, webserver_queue)
+                processed_files.add(file)
 
-        elif input_raw_data[0] == 115:  # test for s character at begingin (Sensor Data)
-            print("Sensor Data")
-            handle_sensor(input_raw_data.decode("utf-8"), webserver_queue)
+        # Pause zwischen den Überprüfungen
+        time.sleep(1)  # Überprüft alle 5 Sekunden
 
-        elif input_raw_data[0] == 120:  # test for x character at beginning (last Image Data)
-            print("last Image Data")
-            handle_image(input_raw_data[1:], True, webserver_queue)
+
+
 
 
 
 def handle_sensor(input_data, webserver_queue):
-    data = input_data.split("|")
+    try:
+        data = input_data.split("|")
 
-    with open("data.json", 'r') as file:
-        json_data = json.load(file)
+        with open("data.json", 'r') as file:
+            json_data = json.load(file)
 
-    json_block = {"send_time": float(data[1]), "receive_time": time.time(), "temperature": float(data[2]),
-                  "humidity": float(data[3]), "pressure": float(data[4]),
-                  "gps_lon": float(data[5]), "gps_lat": float(data[6]), "gps_alt": float(data[7])}
+        json_block = {"send_time": float(data[1]), "receive_time": time.time(), "temperature": string_to_float(data[2]),
+                      "humidity": string_to_float(data[3]), "pressure": string_to_float(data[4]),
+                      "gps_lon": string_to_float(data[9]), "gps_lat": string_to_float(data[10]), "gps_alt": string_to_float(data[8]), "gps_sat": string_to_float(data[11]), "gps_speed": string_to_float(data[12]), "magnetic_lon": string_to_float(data[5]), "magnetic_lat": string_to_float(data[6]), "magnetic_alt": string_to_float(data[7])}
 
-    webserver_queue.put(json_block)
+        webserver_queue.put(json_block)
 
-    json_data['data'].append(json_block)
+        json_data['data'].append(json_block)
 
-    with open("data.json", 'w') as file:
-        json.dump(json_data, file, indent=4)
+        with open("data.json", 'w') as file:
+            json.dump(json_data, file, indent=4)
+    except:
+        print("DATA Error")
 
 
 def handle_image(input_data, last_data_block, webserver_queue):
@@ -80,3 +100,9 @@ def build_image(imageBytes, image_time, webserver_queue):
     print("Decoded Image Size:", len(imageBytes))
     print("Image received and saved successfully!")
     webserver_queue.put({"image": image_time + ".jpg"})
+
+def string_to_float(s):
+    try:
+        return float(s)
+    except ValueError:
+        return 0.0
