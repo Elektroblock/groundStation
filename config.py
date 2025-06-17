8a27ac0c5c89da626002eeeb2c88a37b877b32ba
Julian Buchholz <52217834+Elektroblock@users.noreply.github.com>
2025-06-17 17:32:05 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,14 +3,14 @@ DEBUG = True
 
 # Upload Configuration
 # More information on: https://github.com/Elektroblock/StratoWeb/settings
-use_webclient = False # Enables upload to chosen Server
+use_webclient = True # Enables upload to chosen Server
 api_key = 'XUVCHYbp2y6NMjjELjDERUQtdaLRsTsFshaJgVk585pbcWVT9PuGT7yvm2xHHMXM' # Must be the same betwene Client and Server!
 server_url = "https://live.mlstrato.de"
-input_filepath = 'C:/Users/Arbeit/PycharmProjects/MLS-receive/out'
+input_filepath = 'C:/Users/milvu/Desktop/Strato/MLS-receive-main/out'
 
 # local connection, to remote debug groundStation if device hosting groundstation is not reachable
 # To enable connection to stratoClient, a simple Shell Program to display live Data and see the groundStation logs
 
-use_debug_server = False
+use_debug_server = True
 debug_server_port = 65432
 debug_server_host = '127.0.0.1'
